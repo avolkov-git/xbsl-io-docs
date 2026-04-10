58b7a409ef0568b5b1155bc6abf4ab51b645e28c
Alexandr <ricko28@ya.ru>
2026-04-10 16:25:55 +0300

    Remove YAML and XBSL example pages

--- docs/examples/_meta.ts
+++ docs/examples/_meta.ts
@@ -2,8 +2,6 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   index: "Обзор",
-  "yaml-xbsl-separate": "Пример YMAL/XBSL в разных окнах",
-  "test-example": "Пример YMAL/XBSL в одном окне",
 };
 
 export default meta;
