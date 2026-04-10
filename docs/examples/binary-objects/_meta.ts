9919f337522e61dea228528e336d1b45b75e83f5
Alexandr <ricko28@ya.ru>
2026-04-11 01:54:40 +0300

    Sync examples docs from site mirror

---
+++ docs/examples/binary-objects/_meta.ts
@@ -0,0 +1,9 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: "Обзор",
+  "create-binary-object-from-base64": "Как создать двоичный объект из Base64",
+  "create-base64-from-binary-object": "Как создать Base64 из двоичного объекта",
+};
+
+export default meta;
--- docs/examples/forms/_meta.ts
+++ docs/examples/forms/_meta.ts
@@ -2,8 +2,6 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   index: "Обзор",
-  "forms": "Работа с формами",
-  "binary-objects": "Работа с двоичными данными",
 };
 
 export default meta;
