63a98e95531b94564263d5fcc4108b6813a027b4
Alexandr <ricko28@ya.ru>
2026-04-17 19:16:40 +0300

    add dates/friendly-format.md

--- docs/examples/_meta.ts
+++ docs/examples/_meta.ts
@@ -2,8 +2,10 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   index: "Обзор",
+  "dates": "Работа с датами",
   "forms": "Работа с формами",
   "binary-objects": "Работа с двоичными данными",
+  
 };
 
 export default meta;
---
+++ docs/examples/dates/_meta.ts
@@ -0,0 +1,8 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: "Обзор",
+  "friendly-format": "Дружелюбный формат",
+};
+
+export default meta;
